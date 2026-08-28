fd4c1ecee063ae8accf916b94dbf794b8ea9156f
Krits03 <wkrdev00@gmail.com>
2026-08-29 00:14:40 +0800

    content: 更新友链信息

--- app/feeds.ts
+++ app/feeds.ts
@@ -12,16 +12,39 @@ export default [
 		// @keep-sorted { "keys": ["date"] }
 		entries: [
 			myFeed,
+			{
+				author: '纸鹿本鹿',
+				sitenick: '纸鹿摸鱼处',
+				title: '纸鹿摸鱼处',
+				desc: '纸鹿至麓不知路，支炉制露不止漉',
+				link: 'https://blog.zhilu.site/',
+				feed: 'https://blog.zhilu.site/atom.xml',
+				icon: 'https://www.zhilu.site/api/avatar.png',
+				avatar: 'https://weavatar.com/avatar/47c0f2e82b76d9b10eb3023df9e02e4e3fdbeaf5b74b842063f207971e7fbe7b?s=160',
+				archs: ['Nuxt', 'Astro','Vercel'],
+				date: '2026-06-06',
+				comment: '开发，技术，生活，杂谈',
+			},
 		],
 	},
 	// #endregion
-	// #region 网上邻居 since 2024
+	// #region 网上邻居 since 2026
 	{
 		name: '网上邻居',
 		desc: '哔哔~~通讯中，欢迎常来串门。',
 		// @keep-sorted { "keys": ["date"] }
 		entries: [
-
+			{
+				author: 'Yulliil',
+				desc: '今日はうまく笑えたかな？',
+				link: 'https://yulliil.moe/blog/',
+				feed: 'https://yulliil.moe/blog/',
+				icon: 'https://yulliil.moe/blog/assets/logo.svg',
+				avatar: 'https://yulliil.moe/blog/assets/yulliil.jpg',
+				archs: ['Vue', '国内 CDN'],
+				date: '2026-08-28',
+				comment: 'Q群友 ~♡',
+			},
 		],
 	},
 	// #endregion
