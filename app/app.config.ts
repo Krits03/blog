9cf8ebdda43e847ef519322f259d8d375c91188c
Krits03 <wkrdev00@gmail.com>
2026-08-31 07:44:22 +0800

    feat: 导航栏新增站点日志入口

--- app/app.config.ts
+++ app/app.config.ts
@@ -114,6 +114,7 @@ export default defineAppConfig({
 				{ icon: 'tabler:files', text: '文章', url: '/' },
 				{ icon: 'tabler:link', text: '友链', url: '/link' },
 				{ icon: 'tabler:archive', text: '归档', url: '/archive' },
+				{ icon: 'tabler:info', text: '站点日志', url: '/site-logs' },
 			],
 		},
 	] satisfies Nav,
